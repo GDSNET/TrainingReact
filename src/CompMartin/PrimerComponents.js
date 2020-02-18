ee84b6939b53a028e7bea25788d22c6175b4028a
mstone <mstone@gdsnet.com>
2020-02-18 10:31:20 -0300

    asd
    asd

---
+++ src/CompMartin/PrimerComponents.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native-web'
+import { Component } from "react";
+
+
+
+class Main extends Component {
+
+
+
+  render(){
+  return (
+      <View>
+          
+            <Text style={styles.container}>Martin</Text>
+          
+          
+          
+    </View>
+    );
+  }
+}
+
+export default Main;
+
+const styles = StyleSheet.create({
+  container: {
+    color:'#e6ffe6'
+  }
+});
