6895ece13c9b5256dd6406aa7400ce6c5bb08625
mstone <mstone@gdsnet.com>
2020-02-18 11:29:13 -0300

    compartiendodATOS
    DATOS

--- src/CompMartin/PrimerComponents.js
+++ src/CompMartin/PrimerComponents.js
@@ -1,30 +1,54 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native-web'
 import { Component } from "react";
+import Data from '../APIS/ArbolesJSON.json'
 
 
 
-class Main extends Component {
+export default class PrimerComponentsMartin extends Component {
 
+  funMuestraDatos(){
+   return Data.map((fila, i)=>{
+
+    return(
+      <View>
+    <Text key={i} style={styles.container}>Supervisor: {fila.supervisor}</Text>
+     {this.funMuestraArboles(fila.arboles)}
+    </View>
+    )
+    })
+
+  }
+
+  funMuestraArboles(arbolitos){
+    return arbolitos.map((fila, i)=>{
+ 
+     return(
+     <Text key={i} style={styles.container}>Arbol: {fila.especie}</Text>
+     )
+     })
+ 
+   }
 
 
   render(){
+
   return (
-      <View>
-          
-            <Text style={styles.container}>Martin</Text>
-          
-          
-          
-    </View>
+  <View>
+            <Text style={styles.container}>Martin Compornent</Text>
+            
+
+            {this.funMuestraDatos()}
+
+ </View>
     );
   }
 }
 
-export default Main;
+
 
 const styles = StyleSheet.create({
   container: {
-    color:'#e6ffe6'
+    color:'#BBB'
   }
 });
